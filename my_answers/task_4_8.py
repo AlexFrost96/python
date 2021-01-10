64903ac4fbdfc7e507646600c2d9853e614b00e1
AlexFrost96 <alexxjoker96@gmail.com>
2021-01-10 23:57:49 +0200

    Task 4_8

---
+++ my_answers/task_4_8.py
@@ -0,0 +1,7 @@
+ip = '10.1.1.1'
+ip_list = list(ip.split('.'))
+print(f'''
+      IP address:
+      {ip_list[0]:<8} {ip_list[1]:<8} {ip_list[2]:<8} {ip_list[3]:<8}
+      {int(ip_list[0]):08b} {int(ip_list[1]):08b} {int(ip_list[2]):08b} {int(ip_list[3]):08b}''')
+
