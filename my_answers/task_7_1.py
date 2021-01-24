be3d92fd8b033f745ce8699137b65085231885e6
AlexFrost96 <alexxjoker96@gmail.com>
2021-01-24 13:01:28 +0200

    7_1 upgrade

--- my_answers/task_7_1.py
+++ my_answers/task_7_1.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
+atribute = ["Prefix",
+            "AD/Metric",
+            "Next-Hop",
+            "Last update",
+            "Outbound Interface"]
 with open('ospf.txt', 'r') as f:
     for line in f:
         line_list = line.replace(',','').split()
         line_list.remove("via")
         line_list.remove("O")
         print(f'''
-        Prefix {line_list[0]:<8}
-        AD/Metric {line_list[1]:<8}
-        Next-Hop {line_list[2]:<8}
-        Last update {line_list[3]:<8}
-        Outbound Interface{line_list[4]:<8}''')
+        {atribute[0]:<20} {line_list[0]:<8}
+        {atribute[1]:<20} {line_list[1]:<8}
+        {atribute[2]:<20} {line_list[2]:<8}
+        {atribute[3]:<20} {line_list[3]:<8}
+        {atribute[4]:<20} Interface{line_list[4]:<8}''')
