c604348a2ab1db875dc2c22c19b2a977a98619d1
AlexFrost96 <alexxjoker96@gmail.com>
2021-01-24 21:48:24 +0200

    +

--- my_answers/task_7_3.py
+++ my_answers/task_7_3.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 filename = input('Введіть назву файлу:')
-
+ignore = ["duplex", "alias", "Current configuration"]
 with open(filename) as f:
         for line in f:
-            if  line.startswith('!') == False:
+            if  line.startswith('!') == False and line.startswith(ignore[0]) == False and line.startswith(ignore[1]) == False and line.startswith(ignore[2]) == False:
                 print(line.rstrip())
             else:
                 continue
             
-                
+
