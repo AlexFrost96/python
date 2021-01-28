4bb6f989e3024511b93f86b94dccfc4ad1dbddc4
AlexFrost96 <alexxjoker96@gmail.com>
2021-01-28 23:51:54 +0200

    task_7_3

--- my_answers/task_7_3.py
+++ my_answers/task_7_3.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
-with open(CAM_table.txt) as f:
+with open('CAM_table.txt') as f:
         for line in f:
-            if   line
-                continue
-            else:
-                k = open(input_save, 'a')
-                k.write(line + '\n') 
-                k.close()
+            str_list = line.split()
+            for arg in str_list:
+                if arg =='DYNAMIC':
+                    str_list.remove('DYNAMIC')
+                    print(f'''{str_list[0]:<7} {str_list[1]:<17} {str_list[2]:<8}''')
+                else:
+                    continue
+                
                        
 
